a7617531409b8786ca2c365964b2fa7aff137fbe
mashtalir-sahsa <mashtalir_sasha@ukr.net>
2023-01-26 22:31:48 +0200

    0.9.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
+	if ($(window).innerWidth() > 991) {
+		const navHeight = $('.nav-head').innerHeight()
+		$('.content-head').css({
+			'height': 'calc(100vh - '+navHeight+'px)',
+		})
+	}
+
 	// Инит фансибокса
 	$('.fancybox').fancybox({
 		margin: 0,
